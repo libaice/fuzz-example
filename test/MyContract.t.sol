7d3529a34cf8721afe3fb11563d3d5d8ef63261c
root <libaice147@gmail.com>
2023-12-26 10:41:03 +0000

    add fuzz test example

--- test/MyContract.t.sol
+++ test/MyContract.t.sol
@@ -1,16 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import  {MyContract} from "../src/MyContract.sol";
-import {Test} from "forge-std/Test.sol";
-
+import {MyContract} from "../src/MyContract.sol";
+import {Test,console} from "forge-std/Test.sol";
 
 contract MyContractTest is Test {
-    MyContract exampleContract = new MyContract();
+    MyContract exampleContract;
+
+    function setUp() public {
+        exampleContract = new MyContract();
+    }
 
     function testIsAlwaysZeroUnit() public {
         uint256 data = 0;
         exampleContract.doStuff(data);
         assert(exampleContract.shouldAlwaysBeZero() == 0);
     }
+
+    function testWrongIsAlwaysZeroUint() public {
+        uint256 data = 2;
+        exampleContract.doStuff(data);
+        assertFalse(exampleContract.shouldAlwaysBeZero() == 0);
+    }
+
+    function testIsAlwaysZeroFuzz(uint256 randomData) public {
+        exampleContract.doStuff(randomData);
+        assert(exampleContract.shouldAlwaysBeZero() == 0);
+    }
+
+    function testIsAlwaysZeroUintManyCalls() public {
+        uint256 data = 7;
+        exampleContract.doStuff(data);
+        assert(exampleContract.shouldAlwaysBeZero() == 0);
+
+        data = 0;
+        exampleContract.doStuff(data);
+        assert(exampleContract.shouldAlwaysBeZero() == 0);
+    }
+
+
+    
+
 }
+
